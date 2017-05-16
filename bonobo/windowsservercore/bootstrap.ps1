279d8eef4e5706259324c589bcfd66d62721d1b1
Elton Stoneman <elton@sixeyed.com>
2017-05-16 10:27:30 +0100

    Added Bonobo Git Server

--- bonobo/windowsservercore/bootstrap.ps1
+++
@@ -1,7 +0,0 @@
-
-#.\Set-OwnerAcl.ps1 -Path $env:REPOSITORIES_PATH -Owner 'BUILTIN\IIS_IUSRS'
-
-
-.\Set-OwnerAcl.ps1 -Path 'C:\bonobo\Bonobo.Git.Server\App_Data\Repos' -Owner 'BUILTIN\IIS_IUSRS'
-
-& C:\ServiceMonitor.exe w3svc
