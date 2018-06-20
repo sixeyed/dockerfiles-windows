84a117d53678013c4c4a4d3d4c92c026c424769d
Elton Stoneman <elton@sixeyed.com>
2018-06-20 15:04:17 +0100

    Add build args

--- build-tag-push.ps1
+++ build-tag-push.ps1
@@ -1,7 +1,8 @@
 param(    
     [string] $imageVersion='',
     [string] $registryUser='sixeyed',    
-    [string] $localRegistry='registry.sixeyed:5000'    
+    [string] $localRegistry='registry.sixeyed:5000',    
+    [string] $buildArgs=''
 )
 
 # builds and pushes Docker images
@@ -9,6 +10,11 @@ param(
 #   - directory structure is: {image}/{os}/{os-branch}, e.g. jenkins/windowsservercore/ltsc2016
 #   - Dockerfile and context are in the {os-branch} directory
 #   - this command is executed from the {os-branch} directory
+# input:
+#   - imageVersion - version of the image being built
+#   - regsitryUser - registry username or organization
+#   - localRegistry - registry push destination, in addition to Docker Hub
+#   - buildArgs - comma separated values to pass as `build-arg` in Docker build
 # output:
 #   - image built and pushed with multiple tags using the format {user}/{image}:{version-{os}-{os-branch},
 #   - e.g. 
@@ -22,9 +28,16 @@ $count = $path.Length
 $branchName = $path[$count-1]
 $osName = $path[$count-2]
 $imageName = $path[$count-3]
-
 $fullTag = "$($registryUser)/$($imageName):$($imageVersion)-$($osName)-$($branchName)"
-& docker $config image build -t $fullTag .
+
+$buildArg=@()
+$buildArgsExpanded=$buildArgs.Split(',')
+foreach ($arg in $buildArgsExpanded){
+    $buildArg += "--build-arg", $arg
+}
+
+Write-Host "** Building image: $fullTag, with args: $buildArg"
+& docker $config image build $buildArg -t $fullTag .
 
 $tags = @($fullTag,
           "$($registryUser)/$($imageName):$($osName)-$($branchName)",
