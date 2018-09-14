7c5d6e38495480ca05788156defed9661baa4541
Elton Stoneman <elton@sixeyed.com>
2018-09-14 11:01:21 +0100

    Add whoami

---
+++ whoami-dotnet/src/whoami/Controllers/WebController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace whoami.Controllers
+{
+    [Route("/")]
+    [Controller]
+    public class WebController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        { 
+            var host = Dns.GetHostName();
+            var osDescription = RuntimeInformation.OSDescription;
+            return $"I'm {host} running on {osDescription}";
+        }
+    }
+}
